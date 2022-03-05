15d7b94eb6b2a6e7ab5c2f7dfee7d3d321d7b75f
stef <stefrcn@gmail.com>
2022-03-05 02:11:51 +0100

    Primer commit

---
+++ src/com/OpenB/Project1.java
@@ -0,0 +1,14 @@
+package com.OpenB;
+
+public class Project1 {
+
+    public static void main(String[] args) {
+        int numero1 = 5;
+        System.out.println(numero1);
+
+        numero1 = 10;
+        System.out.println("variable modificada es:" + numero1);
+
+    }
+}
+
---
+++ src/com/OpenB/Project2.java
@@ -0,0 +1,12 @@
+package com.OpenB;
+
+public class Project2 {
+
+    public static void main(String[] args) {
+        //Array de 4 elementos
+        int[] Array={3,23,34,4};
+
+        System.out.println(Array[0]);
+
+    }
+}
